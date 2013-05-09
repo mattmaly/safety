32e7887a924f9f5e64aff910d6dd8d09f5105522
Matt Maly <matthew.r.maly@gmail.com>
2013-05-09 18:56:06 -0500

    changed representation of automaton edges

--- src/safety/automaton.h
+++ src/safety/automaton.h
@@ -10,21 +10,15 @@
 class Automaton {
 public:
     ~Automaton();
-    void addTransition(Formula* src, const World& label, const Formula* dest);
+    void addTransition(Formula* src, const World& label, Formula* dest);
     void setAccepting(const Formula* f, bool acc);
     void addInitial(const Formula* f);
     void write(std::ostream& out, bool withStateLabels = true);
     
 protected:
-    struct Edge {
-        Edge(const World& w, Formula* f);
-        World world;
-        Formula* dest;
-    };
-
     void freeMemory();
 
-    std::map<Formula*, std::vector<Edge> > edges;
+    std::map<Formula*, std::map<Formula*, std::vector<World> > > edges;
     std::map<Formula*, bool> accepting;
     std::vector<Formula*> initial;
 };
--- src/safety/src/automaton.cpp
+++ src/safety/src/automaton.cpp
@@ -9,13 +9,14 @@ Automaton::~Automaton() {
     freeMemory();
 }
 
-void Automaton::addTransition(Formula* src, const World& label, const Formula* dest) {
-    Edge edgeToAdd(label, dest->copy());
-    std::map<Formula*, std::vector<Edge> >::iterator s = edges.find(src);
-    if (s == edges.end())
-        edges[src->copy()].push_back(edgeToAdd);
-    else
-        (s->second).push_back(edgeToAdd);
+void Automaton::addTransition(Formula* src, const World& label, Formula* dest) {
+    typedef std::map<Formula*, std::map<Formula*, std::vector<World> > >::iterator SourceIter;
+    SourceIter s = edges.find(src);
+    src = (s == edges.end() ? src->copy() : s->first);
+    typedef std::map<Formula*, std::vector<World> >::iterator DestIter;
+    DestIter d = edges[src].find(dest);
+    dest = (d == edges[src].end() ? dest->copy() : d->first);
+    edges[src][dest].push_back(label);
 }
 
 void Automaton::setAccepting(const Formula* f, bool acc) {
@@ -30,10 +31,10 @@ void Automaton::write(std::ostream& out, bool withStateLabels) {
     out << "digraph automaton {" << std::endl;
     out << "rankdir=LR" << std::endl;
     std::map<const Formula*, int> formulaIndex;
+    typedef std::map<Formula*, std::map<Formula*, std::vector<World> > >::const_iterator SourceIter;
+    typedef std::map<Formula*, std::vector<World> >::const_iterator DestIter;
 
-    typedef std::map<Formula*, std::vector<Edge> >::const_iterator StateIter;
-    typedef std::vector<Edge>::const_iterator EdgeIter;
-    for (StateIter s = edges.begin(); s != edges.end(); ++s) {
+    for (SourceIter s = edges.begin(); s != edges.end(); ++s) {
         Formula* src = s->first;
         if (formulaIndex.find(src) == formulaIndex.end())
             formulaIndex[src] = formulaIndex.size();
@@ -44,33 +45,42 @@ void Automaton::write(std::ostream& out, bool withStateLabels) {
         if (!withStateLabels)
             out << ",label=\"\"";
         out << "]" << std::endl;
-        const std::vector<Edge>& eVect = s->second;
-        for (EdgeIter e = eVect.begin(); e != eVect.end(); ++e) {
-            const World& label = e->world;
-            Formula* dest = e->dest;
+        const std::map<Formula*, std::vector<World> >& outEdges = s->second;
+        for (DestIter d = outEdges.begin(); d != outEdges.end(); ++d) {
+            const std::vector<World>& label = d->second;
+            Formula* dest = d->first;
             out << "\"";
             src->write(out);
             out << "\" -> \"";
             dest->write(out);
             out << "\" [label=\"";
-            label.write(out);
+            typedef std::vector<World>::const_iterator WorldIter;
+            WorldIter w = label.begin();
+            if (label.size() > 1)
+                out << "(";
+            w->write(out);
+            if (label.size() > 1)
+                out << ")";
+            ++w;
+            for (; w != label.end(); ++w) {
+                out << " | (";
+                w->write(out);
+                out << ")";
+            }
             out << "\"]" << std::endl;
         }
     }
     out << "}";
 }
 
-Automaton::Edge::Edge(const World& w, Formula* f) : world(w), dest(f) {
-}
-
 void Automaton::freeMemory() {
-    typedef std::map<Formula*, std::vector<Edge> >::iterator StateIter;
-    typedef std::vector<Edge>::iterator EdgeIter;
-    for (StateIter s = edges.begin(); s != edges.end(); ++s) {
+    typedef std::map<Formula*, std::map<Formula*, std::vector<World> > >::iterator SourceIter;
+    typedef std::map<Formula*, std::vector<World> >::iterator DestIter;
+    for (SourceIter s = edges.begin(); s != edges.end(); ++s) {
         delete s->first;
-        std::vector<Edge>& edgeVect = s->second;
-        for (EdgeIter e = edgeVect.begin(); e != edgeVect.end(); ++e)
-            delete e->dest;
+        std::map<Formula*, std::vector<World> >& outEdges = s->second;
+        for (DestIter d = outEdges.begin(); d != outEdges.end(); ++d)
+            delete d->first;
     }
 
     typedef std::map<Formula*, bool>::iterator AcceptingIter;
