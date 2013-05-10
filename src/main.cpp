827ea735c657f304b97be65bf54f1692465e238a
Matt Maly <matthew.r.maly@gmail.com>
2013-05-09 19:48:46 -0500

    simplified output to just the automaton

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,12 @@
 #include "safety/translator.h"
 
 int main(int argc, char** argv) {
-    std::cout << "input formula: ";
     Formula* f = Formula::parse(std::cin);
-    std::cout << "parsed & simplified formula: ";
     Formula* g = f->simplify();
     delete f;
-    g->write(std::cout);
-    std::cout << std::endl;
 
     Automaton a = Translator::translate(g);    
     delete g;
-    std::cout << "resulting NFA:" << std::endl;
     a.simplifyEdges();
     a.write(std::cout);
     std::cout << std::endl;
