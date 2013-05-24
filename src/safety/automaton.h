e366391db22b5bd7672cde3d0147f4227177461a
Matt Maly <matthew.r.maly@gmail.com>
2013-05-23 23:49:09 -0500

    added docs to formula and automaton headers

--- src/safety/automaton.h
+++ src/safety/automaton.h
@@ -9,15 +9,45 @@
 
 class Automaton {
 public:
+    /** Clears all memory associated with this automaton. */
     ~Automaton();
+
+    /** Adds a transition to this automaton, from the state corresponding
+        to Formula src to the state corresponding to Formula dest,
+        with the edge corresponding to World label.
+        Stores deep copies of both src and dest.
+        Only one deep copy will be made of any given Formula,
+        where equality is measured according to the string
+        representation of the Formula. */
     void addTransition(Formula* src, const World& label, Formula* dest);
+
+    /** Sets the state corresponding to the Formula f to have
+        acceptance condition acc. Stores a deep copy of f.
+        Only one deep copy will be made of any given Formula,
+        where equality is measured according to the string
+        representation of the Formula. */
     void setAccepting(const Formula* f, bool acc);
+
+    /** Adds the state corresponding to the Formula f as an
+        initial state of the automaton. Stores a deep copy of f.
+        Only one deep copy will be made of any given Formula,
+        where equality is measured according to the string
+        representation of the Formula. */
     void addInitial(const Formula* f);
+
+    /** Prints this automaton to a given output stream.
+        If flag withStateLabels is true, then each automaton state
+        is labeled with its corresponding Formula. */
     void write(std::ostream& out, bool withStateLabels = true);
+
+    /** Simplifies the disjunctions along each edge of the automaton. */
     void simplifyEdges();
     
 protected:
+    /** Clears all memory associated with this automaton. */
     void freeMemory();
+
+    /** Simplifies a given disjunction of Worlds. */
     static void simplifyEdge(std::vector<World>& label);
 
     std::map<Formula*, std::map<Formula*, std::vector<World> > > edges;
--- src/safety/formula.h
+++ src/safety/formula.h
@@ -8,22 +8,44 @@
 
 class Formula {
 public:
+    /** Clears all memory associated with this formula. */
     virtual ~Formula();
+
+    /** Returns a newly allocated deep copy of this formula. */
     virtual Formula* copy() const = 0;
+
+    /** Returns a newly allocated simplified copy of this formula. */
     virtual Formula* simplify() const;
+
+    /** Returns, as a newly allocated formula, the result of this
+        formula evaluated at a given world. */
     virtual Formula* evaluate(const World& w) const = 0;
+
+    /** Returns whether this formula is logically true. */
     virtual bool isTrue() const;
+
+    /** Returns whether this formula is logically false. */
     virtual bool isFalse() const;
 
+    /** Returns a newly allocated formula, reading from an input stream. */
     static Formula* parse(std::istream& in);
+
+    /** Returns a newly allocated formula, given an operator. */
     static Formula* getFormulaOfType(const std::string& op);
 
 protected:
+    /** Fills the children of this formula, if any,
+        by reading from an input stream. */
     virtual void read(std::istream& in) = 0;
+
+    /** Prints this formula to a given output stream. */
     virtual void write(std::ostream& out) const = 0;
 
+    /** The string representation of this formula as printed by write(). */
     mutable std::string name;
 
+    /** A customized ostream printing function so that Formula* values
+        are printed as the corresponding Formula object. */
     friend std::ostream& operator<<(std::ostream& out, const Formula* f);
 };
 
@@ -31,6 +53,9 @@ namespace std {
     template <>
     struct less<Formula*> {
     public:
+        /** A customized less-than function so that Formula* values
+            are compared according to the string representations of their
+            corresponding Formula objects. */
         bool operator()(Formula* const& f, Formula* const& g) const;
     };
 }
