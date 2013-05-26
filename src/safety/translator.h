c10688dde50bf60122559a8334f1799816644c2b
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 16:13:39 -0500

    added docs to translator header

--- src/safety/translator.h
+++ src/safety/translator.h
@@ -8,13 +8,23 @@
 
 class Translator {
 public:
+    /** Returns an Automaton that accepts precisely the set of
+        traces of Worlds that satisfy the Formula f. */
     static Automaton translate(const Formula* f);
 
 protected:
+    /** A class that iterates over all possible truth assignments
+        over all propositions that have been registered with World. */
     class WorldIterator {
     public:
+        /** Constructs and initializes a WorldIterator. */
         WorldIterator();
+
+        /** Returns whether this WorldIterator contains remaining Worlds. */
         bool hasNext();
+
+        /** Returns the next World. Behavior is undefined if this WorldIterator
+            is already finished (i.e., if hasNext() returns false). */
         World next();
 
     protected:
