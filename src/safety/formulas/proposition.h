ad11fc486be9166804b64036adbc10b0d001a422
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 17:51:26 -0500

    added docs to proposition header

--- src/safety/formulas/proposition.h
+++ src/safety/formulas/proposition.h
@@ -9,11 +9,26 @@
 
 class Proposition : public Formula {
 public:
+    /** Creates a proposition with a given name. */
     Proposition(const std::string& n);
+
+    /** Clears all memory associated with this proposition. */
     virtual ~Proposition();
+
+    /** Reads nothing from an input stream, since propositions do not
+        have subformulas. Instead, registers this proposition with World. */
     virtual void read(std::istream& in);
+
+    /** Prints this proposition to a given output stream. */
     virtual void write(std::ostream& out) const;
+
+    /** Returns a newly allocated copy of this proposition. */
     virtual Formula* copy() const;
+
+    /** Returns, as a newly allocated formula, the result of this
+        proposition evaluated at a given World. If the World contains
+        this proposition, then a true or false formula is returned accordingly.
+        Otherwise, a copy of this proposition will be returned. */
     virtual Formula* evaluate(const World& w) const;
 
 protected:
