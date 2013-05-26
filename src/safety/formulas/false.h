9487496cbea5a8704e85e1c2433c6b0031af754c
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 16:50:05 -0500

    added docs to false header

--- src/safety/formulas/false.h
+++ src/safety/formulas/false.h
@@ -8,11 +8,24 @@
 
 class False : public Formula {
 public:
+    /** Clears all memory associated with this false formula. */
     virtual ~False();
+
+    /** Reads nothing from an input stream, since false formulas do not
+        have subformulas. */
     virtual void read(std::istream& in);
+
+    /** Prints this false formula to a given output stream. */
     virtual void write(std::ostream& out) const;
+
+    /** Returns a newly-allocated false formula. */
     virtual Formula* copy() const;
+
+    /** Returns a newly-allocated false formula, since false evaluates
+        to false at any given World. */
     virtual Formula* evaluate(const World& w) const;
+
+    /** Returns true to signify that this false formula is logically false. */
     virtual bool isFalse() const;
 };
 
