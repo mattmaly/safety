532973b575e356ddd6348c334d7e52cf68bda85f
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 16:46:04 -0500

    added docs to eventually header

--- src/safety/formulas/eventually.h
+++ src/safety/formulas/eventually.h
@@ -7,11 +7,23 @@
 
 class Eventually : public UnaryFormula {
 public:
+    /** Creates an eventually formula with a given subformula. */
     Eventually(Formula* c = NULL);
+
+    /** Clears the subformula belonging to this eventually formula. */
     virtual ~Eventually();
+
+    /** Returns a newly allocated deep copy of this eventually formula. */
     virtual Formula* copy() const;
+
+    /** Returns a newly allocated simplified copy of this eventually formula. */
     virtual Formula* simplify() const;
+
+    /** Returns, as a newly allocated formula, the result of this
+        eventually formula evaluated at a given world. */
     virtual Formula* evaluate(const World& w) const;
+
+    /** Returns the operator type 'F' for eventually formulas. */
     virtual std::string getType() const;
 };
 
