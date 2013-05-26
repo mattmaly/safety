14ddb936fa8b13309c446b89638abde768bddce2
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 16:37:13 -0500

    added docs to disjunction header

--- src/safety/formulas/disjunction.h
+++ src/safety/formulas/disjunction.h
@@ -8,11 +8,23 @@
 
 class Disjunction : public CommutativeFormula {
 public:
+    /** Creates a disjunction with a given set of subformulas. */
     Disjunction(const std::set<Formula*>& c = std::set<Formula*>());
+
+    /** Clears all subformulas belonging to this disjunction. */
     virtual ~Disjunction();
+
+    /** Returns a newly allocated deep copy of this disjunction. */
     virtual Formula* copy() const;
+
+    /** Returns a newly allocated simplified copy of this disjunction. */
     virtual Formula* simplify() const;
+
+    /** Returns, as a newly allocated formula, the result of this
+        disjunction evaluated at a given world. */
     virtual Formula* evaluate(const World& w) const;
+
+    /** Returns the operator type '|' for disjunctions. */
     virtual std::string getType() const;
 };
 
