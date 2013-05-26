cbfdaee44e27cdd8375dd68cbb452c37697c87a4
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 17:30:41 -0500

    added docs to next header

--- src/safety/formulas/next.h
+++ src/safety/formulas/next.h
@@ -7,11 +7,23 @@
 
 class Next : public UnaryFormula {
 public:
+    /** Creates an next formula with a given subformula. */
     Next(Formula* c = NULL);
+
+    /** Clears the subformula belonging to this next formula. */
     virtual ~Next();
+
+    /** Returns a newly allocated deep copy of this next formula. */
     virtual Formula* copy() const;
+
+    /** Returns a newly allocated simplified copy of this next formula. */
     virtual Formula* simplify() const;
+
+    /** Returns, as a newly allocated formula, the result of this
+        next formula evaluated at a given world. */
     virtual Formula* evaluate(const World& w) const;
+
+    /** Returns the operator type 'F' for next formulas. */
     virtual std::string getType() const;
 };
 
