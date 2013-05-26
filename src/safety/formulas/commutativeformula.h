31ea94a26794182f66db9c5ff5d4733c8066bf6e
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 16:31:17 -0500

    added docs to commutative formula header

--- src/safety/formulas/commutativeformula.h
+++ src/safety/formulas/commutativeformula.h
@@ -7,13 +7,24 @@
 
 class CommutativeFormula : public Formula {
 public:
+    /** Creates a CommutativeFormula with a given set of subformulas. */
 	CommutativeFormula(const std::set<Formula*>& c = std::set<Formula*>());
+
+    /** Clears all subformulas belonging to this CommutativeFormula. */
 	virtual ~CommutativeFormula();
+
+    /** Returns the operator type for this CommutativeFormula. */
     virtual std::string getType() const = 0;
+
+    /** Returns the set of this CommutativeFormula's subformulas. */
     virtual const std::set<Formula*>& getChildren() const;
 
 protected:
+    /** Fills the subformulas of this CommutativeFormula
+        by reading from an input stream. */
     virtual void read(std::istream& in);
+
+    /** Prints this CommutativeFormula to a given output stream. */
     virtual void write(std::ostream& out) const;
 
     std::set<Formula*> children;
