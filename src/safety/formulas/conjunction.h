d9ced3c6b1e16e5fffbadfcc6f827c4578a600a2
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 16:35:18 -0500

    added docs to conjunction header

--- src/safety/formulas/conjunction.h
+++ src/safety/formulas/conjunction.h
@@ -8,11 +8,23 @@
 
 class Conjunction : public CommutativeFormula {
 public:
+    /** Creates a conjunction with a given set of subformulas. */
     Conjunction(const std::set<Formula*>& c = std::set<Formula*>());
+
+    /** Clears all subformulas belonging to this conjunction. */
     virtual ~Conjunction();
+
+    /** Returns a newly allocated deep copy of this conjunction. */
     virtual Formula* copy() const;
+
+    /** Returns a newly allocated simplified copy of this conjunction. */
     virtual Formula* simplify() const;
+
+    /** Returns, as a newly allocated formula, the result of this
+        conjunction evaluated at a given world. */
     virtual Formula* evaluate(const World& w) const;
+
+    /** Returns the operator type '&' for conjunctions. */
     virtual std::string getType() const;
 };
 
