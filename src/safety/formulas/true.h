acf879ce8b4a226ca0175d68c412001cb4365016
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 16:51:36 -0500

    added docs to true header

--- src/safety/formulas/true.h
+++ src/safety/formulas/true.h
@@ -8,11 +8,24 @@
 
 class True : public Formula {
 public:
+    /** Clears all memory associated with this true formula. */
     virtual ~True();
+
+    /** Reads nothing from an input stream, since true formulas do not
+        have subformulas. */
     virtual void read(std::istream& in);
+
+    /** Prints this true formula to a given output stream. */
     virtual void write(std::ostream& out) const;
+
+    /** Returns a newly-allocated true formula. */
     virtual Formula* copy() const;
+
+    /** Returns a newly-allocated true formula, since true evaluates
+        to true at any given World. */
     virtual Formula* evaluate(const World& w) const;
+
+    /** Returns true to signify that this false formula is logically true. */
     virtual bool isTrue() const;
 };
 
