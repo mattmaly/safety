3f28cd9c6ae9683d1fb5797672f34b7d4050c1ff
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 17:29:19 -0500

    added docs to negation header

--- src/safety/formulas/negation.h
+++ src/safety/formulas/negation.h
@@ -7,11 +7,23 @@
 
 class Negation : public UnaryFormula {
 public:
+    /** Creates an negation with a given subformula. */
     Negation(Formula* c = NULL);
+
+    /** Clears the subformula belonging to this negation. */
     virtual ~Negation();
+
+    /** Returns a newly allocated deep copy of this negation. */
     virtual Formula* copy() const;
+
+    /** Returns a newly allocated simplified copy of this negation. */
     virtual Formula* simplify() const;
+
+    /** Returns, as a newly allocated formula, the result of this
+        negation evaluated at a given world. */
     virtual Formula* evaluate(const World& w) const;
+
+    /** Returns the operator type 'F' for negations. */
     virtual std::string getType() const;
 };
 
