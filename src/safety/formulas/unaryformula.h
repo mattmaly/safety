83f4ef3fc66c3ec72d5a3da13cbf5e3d55ef9cf2
Matt Maly <matthew.r.maly@gmail.com>
2013-05-26 16:41:45 -0500

    added docs to unary formula header

--- src/safety/formulas/unaryformula.h
+++ src/safety/formulas/unaryformula.h
@@ -8,10 +8,20 @@
 
 class UnaryFormula : public Formula {
 public:
+    /** Creates a unary formula with a given subformula. */
     UnaryFormula(Formula* c = NULL);
+
+    /** Clears the subformula belonging to this unary formula. */
     virtual ~UnaryFormula();
+
+    /** Fills the subformula of this unary formula by reading
+        from an input stream. */
     virtual void read(std::istream& in);
+
+    /** Prints this unary formula to a given output stream. */
     virtual void write(std::ostream& out) const;
+
+    /** Returns the operator type for this unary formula. */
     virtual std::string getType() const = 0;
 
 protected:
