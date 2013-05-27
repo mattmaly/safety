9a8b10f5e4d014853904c48c1245f9b30d2310df
Matt Maly <matthew.r.maly@gmail.com>
2013-05-27 17:22:34 -0500

    added docs to until header

--- src/safety/formulas/until.h
+++ src/safety/formulas/until.h
@@ -6,17 +6,37 @@
 
 class Until : public Formula {
 public:
+    /** Creates an until formula with given left and right subformulas. */
     Until(Formula* lChild = NULL, Formula* rChild = NULL);
+
+    /** Clears all subformulas belonging to this until formula. */
     virtual ~Until();
+
+    /** Returns a newly allocated deep copy of this until formula. */
     virtual Formula* copy() const;
+
+    /** Returns a newly allocated simplified copy of this until formula. */
     virtual Formula* simplify() const;
+
+    /** Returns, as a newly allocated formula, the result of this
+        until formula evaluated at a given world. */
     virtual Formula* evaluate(const World& w) const;
+
+    /** Returns the operator type 'U' for until formulas. */
     virtual std::string getType() const;
+
+    /** Returns the left subformula of this until formula. */
     virtual const Formula* getLeft() const;
+
+    /** Returns the right subformula of this until formula. */
     virtual const Formula* getRight() const;
 
 protected:
+    /** Fills the left and right subformulas of this until formula
+        by reading from an input stream. */
     virtual void read(std::istream& in);
+
+    /** Prints this until formula to a given output stream. */
     virtual void write(std::ostream& out) const;
 
     Formula* left;
