994ac1c300ea5058fe632bafd2e7bd315358b4c9
Matt Maly <matthew.r.maly@gmail.com>
2021-06-02 16:11:04 -0700

    Update Conjunction's implementation to C++20 style, to partially address issue #4.

--- src/safety/formulas/conjunction.cpp
+++ src/safety/formulas/conjunction.cpp
@@ -12,20 +12,18 @@ Conjunction::~Conjunction() {
 
 Formula* Conjunction::copy() const {
     std::set<Formula*> copyChildren;
-    typedef std::set<Formula*>::const_iterator ChildIter;
-    for (ChildIter c = children.begin(); c != children.end(); ++c)
-        copyChildren.insert((*c)->copy());
+    for (Formula* c : children)
+        copyChildren.insert(c->copy());
     return new Conjunction(copyChildren);
 }
 
 Formula* Conjunction::simplify() const {
     std::set<Formula*> simpChildren;
-    typedef std::set<Formula*>::const_iterator ChildIter;
-    for (ChildIter c = children.begin(); c != children.end(); ++c) {
-        Formula* sc = (*c)->simplify();
+    for (Formula* c : children) {
+        Formula* sc = c->simplify();
         if (sc->isFalse()) {
-            for (ChildIter cd = simpChildren.begin(); cd != simpChildren.end(); ++cd)
-                delete *cd;
+            for (Formula* cd : simpChildren)
+                delete cd;
             return sc;
         }
         else if (sc->isTrue())
@@ -40,11 +38,11 @@ Formula* Conjunction::simplify() const {
                     delete sc;
             }
             else { //TODO memory leak?
-                for (ChildIter cc = conChild->children.begin(); cc != conChild->children.end(); ++cc) {
-                    if (simpChildren.find(*cc) == simpChildren.end())
-                        simpChildren.insert(*cc);
+                for (Formula* cc : conChild->children) {
+                    if (simpChildren.find(cc) == simpChildren.end())
+                        simpChildren.insert(cc);
                     else
-                        delete *cc;
+                        delete cc;
                 }
                 conChild->children.clear();
                 delete sc; //TODO does this prevent leak or is it overkill?
@@ -60,9 +58,8 @@ Formula* Conjunction::simplify() const {
 
 Formula* Conjunction::evaluate(const World& w) const {
     std::set<Formula*> evalChildren;
-    typedef std::set<Formula*>::const_iterator ChildIter;
-    for (ChildIter c = children.begin(); c != children.end(); ++c) {
-        Formula* evalChild = (*c)->evaluate(w);
+    for (Formula* c : children) {
+        Formula* evalChild = c->evaluate(w);
         if (evalChildren.find(evalChild) == evalChildren.end())
             evalChildren.insert(evalChild);
         else
